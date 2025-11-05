e31d3001313158843a38116739d5a9c031f19e66
MrFrangipane <contact@frangipane.me>
2025-11-05 16:05:48 +0100

    fix(WebServer): move demo to __main__ clause

--- frangipani/web_server/web_server.py
+++ frangipani/web_server/web_server.py
@@ -47,83 +47,100 @@ class WebServer:
         return self._shared_memory_manager.get_all_values()
 
 
-configuration = WebServerConfiguration(
-    public_folder=sys.argv[1],
-    root_control_definition=Group(
-        label="Root Control",
-        placement=Placement(column=0, row=0),
-        controls=[
-            Group(
-                label="Faders",
-                placement=Placement(column=0, row=1, spanColumn=4),
-                controls=[
-                    Fader(
-                        address="/fader1",
-                        label="Fader 1",
-                        placement=Placement(column=0, row=0),
-                        orientation=ControlOrientationEnum.Vertical,
-                        value=0.0
-                    ),
-                    Fader(
-                        address="/fader2",
-                        label="Fader 2",
-                        placement=Placement(column=1, row=0),
-                        orientation=ControlOrientationEnum.Vertical,
-                        value=0.0
-                    ),
-                    Fader(
-                        address="/fader3",
-                        label="Fader 3",
-                        placement=Placement(column=2, row=0),
-                        orientation=ControlOrientationEnum.Vertical,
-                        value=0.5,
-                    ),
-                    Fader(
-                        address="/fader4",
-                        label="Fader 4",
-                        placement=Placement(column=3, row=0),
-                        orientation=ControlOrientationEnum.Vertical,
-                        value=0.5,
-                    ),
-                    Radio(
-                        address="/radio1",
-                        label="Radio 1",
-                        placement=Placement(column=0, row=1, spanColumn=4),
-                        options=["Option 1", "Option 2", "Option 3", "Option 4"],
-                        value=0,
-                    )
-                ]
-            ),
-            ColorWheel(
-                address="/color_wheel",
-                label="Color Wheel",
-                placement=Placement(column=0, row=2),
-                value=(0.0, 0.0)
-            ),
-            Button(
-                address="/button1",
-                label="Button 1",
-                placement=Placement(column=1, row=2),
-                value=False
-            ),
-            Button(
-                address="/button2",
-                label="Button 2",
-                placement=Placement(column=2, row=2),
-                is_toggle=True,
-                value=True
-            ),
-            Button(
-                address="/button3",
-                label="Button 3",
-                placement=Placement(column=3, row=2),
-                is_toggle=True,
-                value=False
-            )
-        ]
-    )
-)
 if __name__ == "__main__":
+    configuration = WebServerConfiguration(
+        public_folder=sys.argv[1],
+        root_control_definition=Group(
+            label="Root Control",
+            placement=Placement(column=0, row=0),
+            controls=[
+                Group(
+                    label="Faders",
+                    placement=Placement(column=0, row=1, spanColumn=4),
+                    controls=[
+                        Fader(
+                            address="/fader1",
+                            label="Fader 1",
+                            placement=Placement(column=0, row=0),
+                            orientation=ControlOrientationEnum.Vertical,
+                            value=0.0
+                        ),
+                        Fader(
+                            address="/fader2",
+                            label="Fader 2",
+                            placement=Placement(column=1, row=0),
+                            orientation=ControlOrientationEnum.Vertical,
+                            value=0.0
+                        ),
+                        Fader(
+                            address="/fader3",
+                            label="Fader 3",
+                            placement=Placement(column=2, row=0),
+                            orientation=ControlOrientationEnum.Vertical,
+                            value=0.5,
+                        ),
+                        Fader(
+                            address="/fader4",
+                            label="Fader 4",
+                            placement=Placement(column=3, row=0),
+                            orientation=ControlOrientationEnum.Vertical,
+                            value=0.5,
+                        ),
+                        Fader(
+                            address="/fader5",
+                            label="Fader 5",
+                            placement=Placement(column=4, row=0),
+                            orientation=ControlOrientationEnum.Vertical,
+                            value=0.5,
+                        ),
+                        Radio(
+                            address="/radio1",
+                            label="Radio 1",
+                            placement=Placement(column=0, row=1, spanColumn=4),
+                            options=["Option 1", "Option 2", "Option 3", "Option 4"],
+                            value=0,
+                        ),
+                        Radio(
+                            address="/radio2",
+                            label="Radio 1",
+                            placement=Placement(column=0, row=2, spanColumn=4),
+                            options=["Option 1", "Option 2"],
+                            value=0,
+                        ),
+                    ]
+                ),
+                ColorWheel(
+                    address="/color_wheel",
+                    label="Color Wheel",
+                    placement=Placement(column=0, row=2),
+                    value=(0.0, 0.0)
+                ),
+                Button(
+                    address="/button1",
+                    label="Button 1",
+                    placement=Placement(column=1, row=2),
+                    value=False
+                ),
+                Button(
+                    address="/button2",
+                    label="Button 2",
+                    placement=Placement(column=2, row=2),
+                    is_toggle=True,
+                    value=True
+                ),
+                Button(
+                    address="/button3",
+                    label="Button 3",
+                    placement=Placement(column=3, row=2),
+                    is_toggle=True,
+                    value=False
+                )
+            ]
+        )
+    )
+
+    with open("server-demo.json", "w") as f:
+        f.write(configuration.root_control_definition.to_json(indent=2))
 
     web_server = WebServer(configuration)
     web_server.start()
