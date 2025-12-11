d227131bb474723e70ee7ea5a8c9f122fd2b0a24
MrFrangipane <contact@frangipane.me>
2025-12-11 02:50:34 +0100

    feat(DriverUpdater): handle inverted drivers /!\ assumes range is always 0 - 100

--- frangipani/engine/driver_updater.py
+++ frangipani/engine/driver_updater.py
@@ -6,14 +6,17 @@ from frangipani.components.components import Components
 
 
 class DriverUpdater:
-
     def __init__(self):
         self._driver_pool_store = Injector().inject(IDriverPoolStore)
         self._web_server = Components().web_server
 
     def read_sources(self) -> None:
+        # FIXME scalars everywhere
         all_values = self._web_server.get_all_values()
 
         for driver in self._driver_pool_store.drivers:
-            source_identifier  = driver.source
-            print(driver.name, all_values[source_identifier.control_address])
+            source_identifier  = driver.source_identifier
+            if driver.inverted:
+                driver.source_value = 100.0 - all_values[source_identifier.control_address]
+            else:
+                driver.source_value = all_values[source_identifier.control_address]
